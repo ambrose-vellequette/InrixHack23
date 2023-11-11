5e2f2d60bc0ddd75c262cad9823dbdbf5c64ab32
Kieran-Greeley <kieran.greeley@gmail.com>
2023-11-11 14:58:44 -0800

    config and index
    python code for config and index

---
+++ Backend/config.py
@@ -0,0 +1,9 @@
+config = {}
+# Setting the application ID. Replace "<your-app-id-here>" with the actual application ID.
+config['appId'] = "<your-app-id-here>"
+# Setting the hash token. Replace "<your-hash-token-here>" with the actual hash token.
+config['hashToken'] = "<your-hash-token-here>"
+# Setting the authentication token URL.
+config['authTokenUrl'] = "https://api.iq.inrix.com/auth/v1/appToken"
+# Printing the configuration (optional)
+print(config)
---
+++ Backend/index.py
@@ -0,0 +1,74 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import requests
+from datetime import datetime, timedelta
+from cachetools import TTLCache
+
+app = Flask(__name__)
+CORS(app)
+
+# Using TTLCache as an equivalent for NodeCache with a TTL of 1 hour
+api_cache = TTLCache(maxsize=1000, ttl=3600)
+
+# Importing config values from a separate module
+from config import config
+
+@app.route('/getToken', methods=['GET'])
+def get_token():
+    # url to produce token based on appId and hashToken
+    response_headers = {'Access-Control-Allow-Origin': '*'}
+
+    # get from .env variables
+    # appId = os.environ.get('APP_ID')  # req.args.get('appId')
+    # hashToken = os.environ.get('HASH_TOKEN')
+
+    app_id = config['appId']
+    hash_token = config['hashToken']
+
+    if app_id in api_cache:
+        # Serve response from cache
+        return jsonify(api_cache[app_id]), 200, response_headers
+    else:
+        # create server request
+        headers = {
+            'content-type': 'application/json',
+            'Accept': 'application/json',
+        }
+
+        params = {
+            'appId': app_id,
+            'hashToken': hash_token,
+        }
+
+        try:
+            response = requests.get(config['authTokenUrl'], params=params, headers=headers)
+            response.raise_for_status()
+        except requests.exceptions.RequestException as err:
+            return jsonify({'error': 'error fetching token'}), 500, response_headers
+
+        if response.status_code == 200:
+            one_hour = 60 * 60  # One hour in seconds
+            exp = (datetime.utcnow() + timedelta(seconds=one_hour)).timestamp()
+
+            payload = {
+                'token': response.json()['result']['token'],
+                'exp': exp,  # in seconds
+            }
+
+            # Set value for the same appId to serve future requests efficiently
+            api_cache[app_id] = payload
+
+            return jsonify(payload), 200, response_headers
+
+@app.route('/checkConfig', methods=['GET'])
+def check_config():
+    exists = all(key in config for key in ('appId', 'hashToken', 'authTokenUrl'))
+    payload = {
+        'exists': exists,
+    }
+    return jsonify(payload), 200, {'Access-Control-Allow-Origin': '*'}
+
+if __name__ == '__main__':
+    port = 3000
+    app.run(port=port)
+    print(f'Open the URL: http://localhost:{port} in your browser')
