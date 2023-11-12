a7c85b799a054ba7557305cf9cf290623184e064
vhuyngo <vinhhuyngo@outlook.com>
2023-11-11 21:43:41 -0800

    Update radius.py

---
+++ Backend/radius.py
@@ -0,0 +1,10 @@
+# call street parking for lat, long
+
+lat = "37.74304518280319"
+long = "-122.42438793182373"
+
+# call crime
+
+for obj in array:
+    if lat - array[i++][0] > -20 &&  lat - array[i++][0] < 20 # lat
+        
