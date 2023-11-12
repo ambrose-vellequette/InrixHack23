713f6d5b2c08d7fa1ea076d14338cad35d975d2b
ambrose-vellequette <ambrose@vellequette.com>
2023-11-12 00:22:45 -0800

    bestpark returns max 3 now

--- backend/bestpark.py
+++ backend/bestpark.py
@@ -20,19 +20,20 @@ def combinator(lat,long):
         print(shit.score)
 
     #max 3 go here
-    maxscores = [0.0,0.0,0.0]
-    for parray in newparkingarray:
-        if parray.score > maxscores[0]:
-            temp = parray.score + maxscores[1:]
-        elif parray.score > maxscores[1]:
-            temp = maxscores[0] + parray.score + maxscores[2]
-        elif parray.score > maxscores[2]:
-            temp = maxscores[0:2] + parray.score
+    maxscores = [newparkingarray[0],newparkingarray[1],newparkingarray[2]]
+    for parray in newparkingarray[3:]:
+        if parray.score > maxscores[0].score:
+            temp = [parray, maxscores[0], maxscores[1]]
+        elif parray.score > maxscores[1].score:
+            temp = [maxscores[0],parray,maxscores[1]]
+        elif parray.score > maxscores[2].score:
+            temp = [maxscores[0],maxscores[1],parray]
         maxscores = temp
-    print(maxscores)
+        print(maxscores[0].score, maxscores[1].score, maxscores[2].score)
+    return maxscores
             
+if __name__ == '__main__':
+    lat = float("37.74304518280319")
+    long = float("-122.42438793182373")
 
-lat = float("37.74304518280319")
-long = float("-122.42438793182373")
-
-combinator(lat,long)
+    a = combinator(lat,long)
