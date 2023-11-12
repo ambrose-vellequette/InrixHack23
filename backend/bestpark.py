ae1931b2561affa39ddc39ea68fdc0e56e239626
ambrose-vellequette <ambrose@vellequette.com>
2023-11-11 22:47:08 -0800

    bestpark fix
    ^

--- backend/bestpark.py
+++ backend/bestpark.py
@@ -1,16 +1,18 @@
 import classes
 from streetParking import findstreetpark
-from crime import crimelist
+from crime import listcrime
 from proximity import checkProximity
 
 def combinator(lat,long):
 
     parking_array = findstreetpark(lat, long)
 
-    crime_array= crimelist()
+    crime_array= listcrime()
 
     checkProximity(parking_array,crime_array)
     
     for shit in parking_array:
         shit.score = shit.prob*(.3*shit.prob -.7*shit.ncrime)
+    
+    #max 3 go here
     
