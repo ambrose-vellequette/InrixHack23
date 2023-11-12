ba2e83ad0eec647bbd3a2939ce9ab8efa3e0da9e
vhuyngo <vinhhuyngo@outlook.com>
2023-11-11 22:43:15 -0800

    Update proximity.py

--- Backend/proximity.py
+++ Backend/proximity.py
@@ -8,6 +8,6 @@ def checkProximity(parking_array, crime_array):
     for crimes in crime_array:
         for streets in parking_array:
             # radius within ___ meters
-            radius = math.sqrt(((parking_array.lat - crime_array.lat) * CONV_DEG_TO_METER)^2 + ((parking_array.long - crime_array.long)* CONV_DEG_TO_METER)^2)
+            radius = math.sqrt(((crimes.lat - streets.lat) * CONV_DEG_TO_METER)^2 + ((crimes.long - streets.long)* CONV_DEG_TO_METER)^2)
             if radius < 20:
                 parking_array.ncrime += 1
