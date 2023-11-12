9bad33717e9e561f386f1f5c0addbe96453ee0d8
vhuyngo <vinhhuyngo@outlook.com>
2023-11-11 23:38:38 -0800

    Update proximity.py

--- Backend/proximity.py
+++ Backend/proximity.py
@@ -3,12 +3,18 @@ import classes
 
 
 def checkProximity(parking_array, crime_array):
-    CONV_DEG_TO_METER = float(111139)
+    #  from Haversine
+    lat_mid = abs((float(crime_array[0].lat) - float(parking_array[0].lat) / 2)
+    CONV_LAT_TO_METER = 111132.954 - 559.822*cos(2.0 * lat_mid) + 1.175*cos(4*lat_mid)
+    CONV_LONG_TO_METER = 111132.954 * 6367449 * cos(lat_mid)
     
     for crimes in crime_array:
         for streets in parking_array:
             # radius within ___ meters
-            radius = math.sqrt(((float(crimes.lat) - float(streets.lat)) * CONV_DEG_TO_METER)**2 + ((float(crimes.long) - float(streets.long))* CONV_DEG_TO_METER)**2)
+            lat_dist = (float(crimes.lat) - float(streets.lat)) * CONV_LAT_TO_METER
+            long_dist = (float(crimes.long) - float(strees.long)) * CONV_LONG_TO_METER
+
+            radius = math.sqrt((lat_dist)**2 + (long_dist)**2)
             if radius <= 100:
                 streets.ncrime += 1
             
